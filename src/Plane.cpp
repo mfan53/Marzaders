f0cb0f2d5f4a6f216d06d5271499664de7112542
JanekLehr <janeklehr@gmail.com>
2013-04-17 18:45:15 -0500

    Added movement functions in the Plane class

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -23,7 +23,7 @@ Plane::Plane(Ogre::SceneManager* mSceneMgr,
 
 	mBody->setRestitution(1);
 	mBody->setActivationState(DISABLE_DEACTIVATION);
-	//mBody->setLinearFactor(btVector3(0,0,1)); // only allow movement on z axis
+	mBody->setLinearFactor(btVector3(1,1,0)); // only allow movement on x,y axis
 	//mBody->setAngularFactor(btVector3(0,0,0)); // Allow no rotations
 
 	mRenderEntity = mSceneMgr->createEntity(name,"RZR-002.mesh");
@@ -38,5 +38,57 @@ Plane::~Plane() {
 }
 
 void Plane::update(float delta) {
+	if (!mMoveUp && !mMoveDown && !mMoveLeft && !mMoveRight) {
+		stop();
+	}
+	else if (mMoveUp) {
+		mBody->setLinearVelocity(btVector3(0.0f, 50.0f, 0.0f);
+	}
+	else if (mMoveDown) {
+		mBody->setLinearVelocity(btVector3(0.0f, -50.0f, 0.0f);
+	}
+	else if (mMoveLeft) {
+		mBody->setLinearVelocity(btVector3(-50.0f, 0.0f, 0.0f);
+	}
+	else if (mMoveRight) {
+		mBody->setLinearVelocity(btVector3(50.0f, 50.0f, 0.0f);
+	}
+
 	Entity::update(delta);
+}
+
+void Plane::moveUp() {
+	mMoveUp = true;
+	mMoveDown = false;
+	mMoveLeft = false;
+	mMoveRight = false;
+}
+
+void Plane::moveDown() {
+	mMoveUp = false;
+	mMoveDown = true;
+	mMoveLeft = false;
+	mMoveRight = false;
+}
+
+void Plane::moveLeft() {
+	mMoveUp = false;
+	mMoveDown = false;
+	mMoveLeft = true;
+	mMoveRight = false;
+}
+
+void Plane::moveRight() {
+	mMoveUp = false;
+	mMoveDown = false;
+	mMoveLeft = false;
+	mMoveRight = true;
+}
+
+void Plane::stop() {
+	mMoveUp = false;
+	mMoveDown = false;
+	mMoveLeft = false;
+	mMoveRight = false;
+	mBody->setLinearVelocity(btVector3(0.0f,0.0f,0.0f));
 }
--- src/Plane.h
+++ src/Plane.h
@@ -9,12 +9,19 @@ namespace Arsenal {
 		Plane(Ogre::SceneManager* mSceneMgr,btDiscreteDynamicsWorld* dynamicsWorld,std::string name);
 		virtual ~Plane();
 		void update(float delta);
+		void moveUp();
+		void moveDown();
+		void moveLeft();
+		void moveRight();
+		void stop();
 
 	private:
 		//player 1 starting pos
 		const static float xcoord = 0.0;
 		const static float ycoord = 0.0;
 		const static float zcoord = 0.0;
+
+		bool mMoveUp, mMoveDown, mMoveLeft, mMoveRight;
 	};
 }
 
