f16192e88954bf3af7fabf493c93ff2ab14d7207
Darwin Pek <darwinpek@gmail.com>
2013-04-16 23:02:15 -0500

    Cleaned up Entity classes

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -3,35 +3,40 @@
 using namespace Arsenal;
 
 Entity::Entity() {
+	// Set the ID of the entity
 	static int next_id = 0;
-	mId = next_id;
+	m_id = next_id;
 	next_id++;
-	mShape = new btBoxShape(btVector3(btScalar(50),btScalar(50),btScalar(50)));
-	mMass = 10;
-	isDynamic = true;
-	mLocalInertia = btVector3(btScalar(0),btScalar(0),btScalar(0));
+
+	// Create the physics components of the entity
+	btScalar mass = 10;
+	btVector3 localInertia = btVector3(btScalar(0),btScalar(0),btScalar(0));
 	btTransform transform;
 	transform.setIdentity();
 	transform.setOrigin(btVector3(0,0,0));
-	mMotionState = new btDefaultMotionState(transform);
+	m_motion = new btDefaultMotionState(transform);
+	m_shape = new btBoxShape(btVector3(btScalar(50),btScalar(50),btScalar(50)));
 	btRigidBody::btRigidBodyConstructionInfo rbInfo
 		= btRigidBody::btRigidBodyConstructionInfo
-			(mMass,mMotionState,mShape,mLocalInertia);
-	mBody = new btRigidBody(rbInfo);
+			(mass,m_motion,m_shape,localInertia);
+	m_body = new btRigidBody(rbInfo);
 }
 
 Entity::~Entity() {
-	dynamicsWorld->removeCollisionObject(mBody);
-	delete mMotionState;	
-	delete mShape;
-	delete mBody;
+	m_dynamics->removeCollisionObject(m_body);
+	delete m_motion;
+	delete m_shape;
+	delete m_body;
 }
 
 void Entity::update(float delta) {
+	// Get the bullet transform
 	btTransform trans;
-	mBody->getMotionState()->getWorldTransform(trans);
-	if(mSceneNode) {
-		mSceneNode->setPosition(
+	m_body->getMotionState()->getWorldTransform(trans);
+
+	// Update the Ogre scene node position
+	if(m_node) {
+		m_node->setPosition(
 			trans.getOrigin().getX(),
 			trans.getOrigin().getY(),
 			trans.getOrigin().getZ());
@@ -39,35 +44,42 @@ void Entity::update(float delta) {
 }
 
 float Entity::getX() {
-	return mBody->getCenterOfMassTransform().getOrigin().getX();
+	return m_body->getCenterOfMassTransform().getOrigin().getX();
 }
 
 float Entity::getY() {
-	return mBody->getCenterOfMassTransform().getOrigin().getY();
+	return m_body->getCenterOfMassTransform().getOrigin().getY();
 }
 
 float Entity::getZ() {
-	return mBody->getCenterOfMassTransform().getOrigin().getZ();
-}
-
-void Entity::setPos(float x, float y, float z) {
-	btTransform transform = mBody->getCenterOfMassTransform();
-	transform.setOrigin(btVector3(x,y,z));
-	mBody->setCenterOfMassTransform(transform);
+	return m_body->getCenterOfMassTransform().getOrigin().getZ();
 }
 
 float Entity::getXV() {
-	return mBody->getLinearVelocity().getX();
+	return m_body->getLinearVelocity().getX();
 }
 
 float Entity::getYV() {
-	return mBody->getLinearVelocity().getY();
+	return m_body->getLinearVelocity().getY();
 }
 
 float Entity::getZV() {
-	return mBody->getLinearVelocity().getZ();
+	return m_body->getLinearVelocity().getZ();
+}
+
+void Entity::setPos(float x, float y, float z) {
+	// Update the bullet position
+	btTransform trans = m_body->getCenterOfMassTransform();
+	trans.setOrigin(btVector3(x,y,z));
+	m_body->setCenterOfMassTransform(trans);
+
+	// Update the Ogre scene node position
+	m_node->setPosition(
+		trans.getOrigin().getX(),
+		trans.getOrigin().getY(),
+		trans.getOrigin().getZ());
 }
 
 void Entity::setVel(float xv, float yv, float zv) {
-	mBody->setLinearVelocity(btVector3(xv,yv,zv));
+	m_body->setLinearVelocity(btVector3(xv,yv,zv));
 }
--- src/Entity.h
+++ src/Entity.h
@@ -8,38 +8,42 @@
 
 namespace Arsenal {
 	class Entity {
-	protected:
-		btDiscreteDynamicsWorld* dynamicsWorld;
-		Ogre::SceneNode* mSceneNode;
-		
-		Ogre::Entity* mRenderEntity;
-		btRigidBody* mBody;
-		btCollisionShape* mShape;
-		btDefaultMotionState * mMotionState;
-		btVector3 mLocalInertia;
-		btScalar mMass;
-		bool isDynamic;
-		int mId;
-
 	public:
 		Entity();
 		virtual ~Entity();
 		
 		virtual void update(float delta);
-		Ogre::SceneNode* getSceneNode() { return mSceneNode; };
-		int getId() {return mId;}
 
+		int getId() { return m_id; }
 		float getX();
 		float getY();
 		float getZ();
-		void setPos(float x, float y, float z);
 		float getXV();
 		float getYV();
 		float getZV();
+		Ogre::SceneNode* getNode() { return m_node; };
+
+		void setPos(float x, float y, float z);
 		void setVel(float vx, float vy, float vz);
 
+	protected:
+		// The id of the Entity
+		int m_id;
+
+		// A reference to the scene node that this Entity belongs to
+		Ogre::SceneNode* m_node;
+		// A reference to the dynamics world that this Entity belongs to
+		btDiscreteDynamicsWorld* m_dynamics;
+
+		// The Ogre render component
+		Ogre::Entity* m_render;
+		// The bullet physics components
+		btRigidBody* m_body;
+		btCollisionShape* m_shape;
+		btDefaultMotionState* m_motion;
 	};
 }
+
 
 #endif // INC_ARSENAL_ENTITY_H
 
