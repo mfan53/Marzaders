2efbf7ca8ee59ab4075c94e54018798ba1836313
JanekLehr <janeklehr@gmail.com>
2013-04-17 16:45:57 -0500

    Changed sound manager to a static class

--- src/sound/SoundManager.h
+++ src/sound/SoundManager.h
@@ -1,67 +1,64 @@
-#ifndef INC_SOUNDMANAGER_H
-#define INC_SOUNDMANAGER_H  
+#ifndef INC_ARSENAL_SOUNDMANAGER_H
+#define INC_ARSENAL_SOUNDMANAGER_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
-//#include <hash_map>
 #include "SDL.h"
 #include "SDL_mixer.h"
 #include "Sound.h"
 
-#define MUS_PLASMA "../../res/music/plasma.ogg"
-#define MUS_SWINGIN_FERRET "../../res/music/cable-swingin-ferret.ogg"
-#define SND_BASS_DRUM "../../res/sounds/808-bassdrum.wav"
-#define SND_CLAP "../../res/sounds/808-clap.wav"
+#define MUS_PLASMA "../res/music/plasma.ogg"
+#define MUS_SWINGIN_FERRET "../res/music/cable-swingin-ferret.ogg"
+#define SND_BASS_DRUM "../res/sounds/808-bassdrum.wav"
+#define SND_CLAP "../res/sounds/808-clap.wav"
 #define SND_HI_HAT "../res/sounds/808-hihat.wav"
-#define SND_PIANO_HIT "../../res/sounds/pianohit.wav"
-#define SND_BEEP "../../res/sounds/beep.wav"
-#define SND_EXPLOSION "../../res/sounds/explosion.wav"
-#define SND_METAL_PLATE "../../res/sounds/metalplate.wav"
-#define SND_POP "../../res/sounds/pop.wav"
+#define SND_PIANO_HIT "../res/sounds/pianohit.wav"
+#define SND_BEEP "../res/sounds/beep.wav"
+#define SND_EXPLOSION "../res/sounds/explosion.wav"
+#define SND_METAL_PLATE "../res/sounds/metalplate.wav"
+#define SND_POP "../res/sounds/pop.wav"
 
 using namespace std;
 namespace Arsenal {
-	class SoundManager {
-		protected:
-			//hash_map <char *, Sound*>;
-			// TODO: replace the vector of sounds with a hashmap
-			vector<Sound *> mSounds;
-			int audio_rate;
-			Uint16 audio_format;
-			int audio_channels;
-			int audio_buffers;
-			int mNumChannels;
-			int mCurrentChannel;
+    class SoundManager {
+        protected:
+            static vector<Sound *> mSounds;
+            static int audio_rate;
+            static Uint16 audio_format;
+            static int audio_channels;
+            static int audio_buffers;
+            static int mNumChannels;
+            static int mCurrentChannel;
 
-			void init(int channels);
-			static void musicDone();
-			static void channelFinished(int channel);
-		public:
-			SoundManager();
-			SoundManager(int numChannels);
-			virtual ~SoundManager();
+            static void musicDone();
+            static void channelFinished(int channel);
+        public:
+            //SoundManager();
+            //SoundManager(int numChannels);
+            //virtual ~SoundManager();
 
-			Sound *createSound(const char *file);
-			/*
-			 * 0 <= vol <= 128
-			 * sets volume for all sounds
-			*/
-			void setSoundVolume(int vol);
-			int loadMusic(const char *file);
-			/* numTimes = -1 -> Loop continuously, otherwise intuitive */
-			void playMusic(int numTimes);
-			void pauseMusic();
-			void resumeMusic();
-			void switchMusic(const char *file, int numTimes);
-			void stopMusic(bool fade);
-			/* 0 <= vol <= 128 */
-			void setMusicVolume(int vol);
-			void closeAudio();
-			int getNumChannels() {return mNumChannels;}
+            static void init(int channels);
+            static Sound *createSound(const char *file);
+            /*
+             * 0 <= vol <= 128
+             * sets volume for all sounds
+            */
+            static void setSoundVolume(int vol);
+            static int loadMusic(const char *file);
+            /* numTimes = -1 -> Loop continuously, otherwise intuitive */
+            static void playMusic(int numTimes);
+            static void pauseMusic();
+            static void resumeMusic();
+            static void switchMusic(const char *file, int numTimes);
+            static void stopMusic(bool fade);
+            /* 0 <= vol <= 128 */
+            static void setMusicVolume(int vol);
+            static void closeAudio();
+            static int getNumChannels() {return mNumChannels;}
 
-			static Mix_Music *music;
-	};
+            static Mix_Music *music;
+    };
 }
 
-#endif // INC_SOUNDMANAGER_H
+#endif // INC_ARSENAL_SOUNDMANAGER_H
